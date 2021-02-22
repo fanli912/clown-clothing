9d2159ede1e84f5bdee377fc721c5228966227ae
fanli912 <lifan@usc.edu>
2021-02-21 22:50:11 -0800

    redux done;

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -13,3 +13,19 @@ export const addItemToCart = (cartItem, cartItemToAdd) => {
 
   return [...cartItem, {...cartItemToAdd, quantity : 1}]
 }
+
+export const removeItemFromCart = (cartItems, cartItemtoRemove) => {
+  const existingCartItem = cartItems.find(
+    cartItem => cartItem.id === cartItemtoRemove.id
+  );
+
+  if(existingCartItem.quantity == 1) {
+    return cartItems.filter(cartItem => cartItem.id !== cartItemtoRemove.id)
+  }
+  return cartItems.map(
+    cartItem =>
+    cartItem.id === cartItemtoRemove.id?
+    {...cartItem, quantity: cartItem.quantity - 1}
+    : cartItem
+  )
+}
