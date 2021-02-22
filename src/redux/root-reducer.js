49983f69f01b17dfcbb68fd50fa82303235b5ad3
fanli912 <lifan@usc.edu>
2021-02-21 23:10:21 -0800

    added reduex storage

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,13 +1,22 @@
 //an object that combine all states together
 import {combineReducers} from 'redux';
-
+import {persistReducer} from 'redux-persist'
 import userReducer from './user/user.reducer'
 import cartReducer from './cart/cart.reducer'
+import storage from 'redux-persist/lib/storage'
 
-export default combineReducers ({
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['cart'] //what reducer you want to persist
+}
+
+const rootReducer = combineReducers ({
   user: userReducer,
   cart: cartReducer
 })
+
+export default persistReducer(persistConfig, rootReducer)
 
 
 //user reducer: store states of current user
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,5 @@
 import {createStore, applyMiddleware} from 'redux';
+import {persistStore} from 'redux-persist'
 
 //middleware: receive action in, do something with them and pass in to root reducer
 
@@ -7,6 +8,9 @@ import rootReducer from './root-reducer'
 
 const middlewares = [logger];
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
+export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
-export default store;
+export const persistor = persistStore(store);
+
+
+export default {store, persistor};
